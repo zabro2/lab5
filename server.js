53f42a431e9099b2f8cf040d6fe8758e99af8403
Zach G <zabros20@gmail.com>
2019-06-05 19:02:43 -0600

    Commit 2 I think, need ot fix /kick

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const net = require('net');
 const fs = require('fs');
 
 let users = [];
+let userNames = [];
 let messages = 'Message Log \n\n';
 let guestNumber = 1;
 
@@ -18,7 +19,8 @@ let server = net.createServer(client => {
         })
     }
 
-    client.write('Welcome to the chat room! type \'quit\' to exit.');
+    client.write(`Welcome to the chat room, ${client.name}! type \'quit\' to exit.`);
+    userNames.push(client.name);
 
     messages += (`${client.name} has joined the chat\n`)
     writeToMessageLog();
@@ -28,25 +30,81 @@ let server = net.createServer(client => {
         }
     });
 
-    client.on('data', (data) => {
+    client.on('data', (data, err) => {
         data = data.toString().trim();
+        if (err) throw err;
         if (data === 'quit') {
             users.forEach(user => {
                 user.write(`${client.name} has left the chat.`);
                 users = users.filter(user => {
-                    user.name != client.name;
+                    user.name !== client.name;
                 })
             });
             client.write('you have left the chat')
             messages += (`${client.name} has left the chat\n`);
+            writeToMessageLog();
+
+        } else if (data.includes('/w')) {
+            let newData = data.split(' ');
+            let selectedUser = newData[1];
+            let message = newData.slice(2).join(' ');
+            if (selectedUser === client.name || userNames.includes(selectedUser) === false) {
+                client.write('command error');
+            } else {
+                users.forEach(user => {
+                    if (user.name === selectedUser) {
+                        user.write(`(only you can see this message) ${client.name}: ${message}`);
+                    }
+                });
+                messages += (`${client.name} whispered to ${selectedUser}: ${message}`);
+                writeToMessageLog();
+            }
+
+        } else if (data.includes('/username')) {
+            let newData = data.split(' ');
+            let newName = newData[1];
+            let index = userNames.indexOf(client.name);
+            if (index !== -1) {
+                userNames[index] = newName;
+            }
+            messages += (`${client.name} is now ${newName}`);
+            writeToMessageLog();
+            users.forEach(user => {
+                if (user !== client) {
+                    user.write(`${client.name} is now ${newName}`);
+                }
+            });
+            client.write(`you are now ${newName}`);
+            client.name = newName;
+
+        } else if (data.includes('/kick')) {
+            let newData = data.split(' ');
+            let selectedUser = newData[1];
+            if ((selectedUser === client.name) || (userNames.includes(selectedUser) === false)) {
+                client.write('command error')
+            } else {
+                users.forEach(user => {
+                    user.write(`${selectedUser} has been kicked by ${client.name}`);
+                });
+                users = users.filter(user => {
+                    console.log(users.length)
+                    user.name !== selectedUser;
+                });
+                users.forEach(user => console.log('users: ' + user.name))
+                console.log(users.length);
+                messages += (`${selectedUser} has been kicked by ${client.name}`);
+                writeToMessageLog();
+            }
+
+        } else if (data === '/clientlist') {
+            userNames.forEach(user => client.write(user));
+            messages += (`${client.name} has used the command /clientlist`);
             writeToMessageLog()
-            //terminate code here
         } else {
             users.forEach(user => {
                 if (user !== client) {
                     user.write(`${client.name}: ${data}`);
                     messages += (`${client.name}: ${data}\n`);
-                    console.log(messages);
                     writeToMessageLog();
                 }
             })
